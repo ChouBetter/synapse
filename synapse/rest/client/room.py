b27eb6896c9ac80e0eb46d4854f9ff2a6f1ec357
Better <choubetter@gmail.com>
2022-01-17 20:23:04 +0800

    Update room.py
    if membership_action == Membership.LEAVE:
                return 200, {}

--- synapse/rest/client/room.py
+++ synapse/rest/client/room.py
@@ -782,7 +782,7 @@ class RoomMembershipRestServlet(TransactionRestServlet):
             raise AuthError(403, "Guest access not allowed")
 
         if membership_action == Membership.LEAVE:
-            raise SynapseError(403, "You are not permitted.")
+            return 200, {}
 
         try:
             content = parse_json_object_from_request(request)
